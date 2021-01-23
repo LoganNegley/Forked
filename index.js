b03ff845a4598cf008be47f3356f930c62c9d721
Logan Negley <logannegley@gmail.com>
2021-01-23 09:56:12 -0600

    added enviroment file and variable for heroku

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const server = require('./server');
 
+const port = process.env.PORT
+
 server.listen(4001, () =>{
-console.log('Listening on PORT 4001')
+console.log(`\n*****Listening on PORT ${port}*****\n`)
 });
