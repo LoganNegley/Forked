df59988d135c4b732b5b26343b06b00873c38e8b
Logan Negley <logannegley@gmail.com>
2021-01-23 10:08:14 -0600

    heroku config done and server running

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const server = require('./server');
 
-const port = process.env.PORT
+const port = process.env.PORT || 5000;
 
-server.listen(4001, () =>{
+server.listen(port, () =>{
 console.log(`\n*****Listening on PORT ${port}*****\n`)
 });
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
-
 const server = express();
 
 server.use(express.json());
 
 // Routes
+
+server.get('/', (req, res) =>{
+res.status(200).json({message: 'Api up and running'})
+});
 
 module.exports = server
