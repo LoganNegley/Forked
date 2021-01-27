1ba0f60950c421d7dcd60b4629240425505a07b3
Logan Negley <logannegley@gmail.com>
2021-01-27 13:42:51 -0600

    add knex cleanup for seeds

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,13 +1,9 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('users').truncate()
-    .then(function () {
-      // Inserts seed entries
-      return knex('users').insert([
-        {first_name: 'Logan', last_name:'Negley',email:'logannegley@gmail.com', username:'ElemenPhoenix', password:'myPassword'},
+exports.seed = function(knex, Promise) {
+  // Inserts seed entries
+    return knex('users').insert([
+      {first_name: 'Logan', last_name:'Negley',email:'logannegley@gmail.com', username:'ElemenPhoenix', password:'myPassword'},
 
-        {first_name: 'Heather', last_name:'Cash',email:'hlcash88@gmail.com', username:'HCash', password:'password'}
-      ]);
-    });
+      {first_name: 'Heather', last_name:'Cash',email:'hlcash88@gmail.com', username:'HCash', password:'password'}
+    ]);
 };
