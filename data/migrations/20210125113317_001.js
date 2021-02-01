b074b758695f566e27489577dd5fbe03bea92002
Logan Negley <logannegley@gmail.com>
2021-02-01 16:23:30 -0600

    migration rolled back to add more tables

--- data/migrations/20210125113317_001.js
+++ data/migrations/20210125113317_001.js
@@ -1,6 +1,8 @@
-
+// Users table build
 exports.up = function(knex) {
-  return knex.schema.createTable('users', tbl =>{
+  return( 
+    knex.schema
+    .createTable('users', tbl =>{
       tbl.increments('id');
       tbl.string('first_name').notNullable();
       tbl.string('last_name').notNullable();
@@ -8,7 +10,24 @@ exports.up = function(knex) {
       tbl.string('username').unique().notNullable();
       tbl.string('password').notNullable();
   })
-};
+    .createTable('cart', tbl =>{
+      tbl.increments('id');
+    })
+)};
+
+
+// user cart table
+// usercart item table
+// Recipes table
+// Recipe_ingredient table
+// Recipe ingredients table
+// Recipes steps table
+// public recipes
+// favorite recipes
+
+
+
+
 
 exports.down = function(knex) {
   return knex.schema.dropTableIfExists('users')
