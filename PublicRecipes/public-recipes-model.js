f9b7005f4c6964f85871cc64cc009cb623a9e5a5
Logan Negley <logannegley@gmail.com>
2021-02-26 16:57:40 -0600

    get all public recipes route and model

--- PublicRecipes/public-recipes-model.js
+++ PublicRecipes/public-recipes-model.js
@@ -2,13 +2,23 @@ const db = require('../data/db-config');
 
 function getAllPublicRecipes(){
     return db('recipes as r')
-
     .join('publicRecipes', 'recipe_id', '=', 'recipeId' )
-
     .select('recipe_id', 'recipeName', 'image','prep_time', 'cook_time', 'r.userId', 'dateAdded')
+};
+
+function getPublicRecipeByUserId(id){
+    return db('publicRecipes as pr')
+    .join('recipes as r', 'pr.recipeId', '=', 'r.recipe_id')
+    .select('r.recipe_id', 'r.recipeName', 'r.image', 'r.prep_time', 'r.cook_time', 'r.userId', 'pr.dateAdded')
+    .where('r.userId', id)
+};
+
+function addPublicRecipe(){
 
 };
 
 module.exports ={
     getAllPublicRecipes,
+    getPublicRecipeByUserId,
+    addPublicRecipe,
 }
