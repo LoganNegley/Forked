b6593b7a1de4ce626860ee9b3730d28b5d3a13db
Logan Negley <logannegley@gmail.com>
2021-03-01 18:27:49 -0600

    post and delete ingredient model and routes

--- Ingredients/ingredient-router.js
+++ Ingredients/ingredient-router.js
@@ -49,32 +49,53 @@ router.get('/:id', (req,res) =>{
 });
 
 // add ingredient to recipe by Id
-router.post('/recipe/:id',(req,res) =>{
+router.post('/recipe/:id', (req,res) =>{
     const {id} = req.params;
     const newIng = req.body;
-    console.log(newIng, 'from req.body')
 
-    if(!newIng.ingredient_name || !newIng.quantity){
-        res.status(404).json({message:'Must fill out all fields'})
-    } else{
-        db.addIngredient(newIng)
-        .then(ing =>{
-            db.addIngredientToRecipe(ing, id)
-            .then(item =>{
-                res.status(201).json({created:item})
+    recipeModel.findRecipeById(id) //validate recipe exists
+    .then(recipe =>{
+        if(recipe.length > 0){
+
+            if(!newIng.ingredient_name || !newIng.quantity){
+                res.status(404).json({message:'Must fill out all fields'})
+        } else{
+            db.addIngredient(newIng) //add ingredient to database
+            .then(ing =>{
+                db.addIngredientToRecipe(ing, id) // adding data to reference table
+                .then(item =>{
+                    res.status(201).json({created:item})
+                })
+                .catch(error =>{
+                    console.log(error)
+                    res.status(500).json({errorMessage:'Failed to add ingredient to recipe'})
+                })
             })
             .catch(error =>{
                 console.log(error)
-                res.status(500).json({errorMessage:'Failed to add ingredient to recipe'})
+                res.status(500).json({errorMessage:'Failed to add ingredient'})
             })
-        })
-        .catch(error =>{
-            console.log(error)
-            res.status(500).json({errorMessage:'Failed to add ingredient'})
-        })
+        }
+            } else{
+                res.status(404).json({message:'Unable to find recipe with that Id'})
+            }
+    })
 
-    }
+    .catch(error =>{
+        res.status(500).json({errorMessage:'Failed to get recipe by Id'})
+    })
+});
 
+router.delete('/:id', (req,res) =>{
+    const {id} = req.params;
+
+    db.deleteIngredientById(id)
+    .then(deleted =>{
+        res.status(200).json({Removed: deleted})
+    })
+    .catch(error =>{
+        res.status(500).json({errorMessage:'Failed to delete ingredient'})
+    })
 });
 
 module.exports = router;
