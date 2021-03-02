2735633aa3550dc2981036bfc2df0b5162615055
Logan Negley <logannegley@gmail.com>
2021-03-01 18:53:25 -0600

    cart items route and model files done

---
+++ CartItems/cartItems-model.js
@@ -0,0 +1,9 @@
+const db = require('../data/db-config');
+
+function getAllCartItems(){
+
+};
+
+module.exports ={
+    getAllCartItems,
+}
---
+++ CartItems/cartItems-router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const db = require('./cartItems-model');
+
+// Get all cart Items
+router,get('/', (req,res) =>{
+
+});
+
+module.exports = router;
