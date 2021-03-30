e02d77046958ef4b36cd4478b1d8ba1fc6b0eba8
Logan Negley <logannegley@gmail.com>
2021-03-30 15:07:07 -0500

    hero section background image added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import headingImage from './assets/images/traced_salad_pic-website';
+
 
 function App() {
   return (
     <div className="App">
-      <img src={headingImage} alt='salad-heading-image'/>
     </div>
   );
 }
