71d03c46eab33941e6a40b0f03ebeaeba3796103
Logan Negley <logannegley@gmail.com>
2021-04-08 13:04:40 -0500

    trying to get menu button operating correctly

--- client/src/components/common/navigation/Navbar.js
+++ client/src/components/common/navigation/Navbar.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navbar.css';
 
 
 function Navbar(){
+    const [toggled, setToggled] = useState(false)
+
+    const toggleMenu = ()=>{
+        console.log('button clicked')
+        if(toggled === false){
+            setToggled(true)
+        }else{
+            setToggled(false)
+        }
+            
+    };
+    console.log(toggled)
+
+
     return (
         <div className='navbar-container'>
-            <button>
-                <img className='burger-menu-icon' src='images/menu-icon.png' alt='burger-menu-icon'/>
+            <button 
+            className='button' 
+            onClick={toggleMenu}>
+                <img className='burger-menu-icon' src='images/menuIcon.png' alt='burger-menu-icon'
+                />
             </button>
             <h1 className='logo'>FORKED</h1>
+
             <div className='nav-links'>
                 <ul>
                     <li>Home</li>
@@ -22,3 +40,4 @@ function Navbar(){
 };
 
 export default Navbar;
+
