aa698b23741507c4cc7be1cf0be27f409615c416
Logan Negley <logannegley@gmail.com>
2021-04-14 13:37:43 -0500

    changed seeds and migration

--- data/seeds/008-publicRecipes.js
+++ data/seeds/008-publicRecipes.js
@@ -3,37 +3,37 @@ exports.seed = function(knex, Promise) {
       return knex('publicRecipes').insert([
         {
         userId: 1,
-        recipeId: 1,
-        dateAdded: 01/22/2019},
+        recipeId: 1
+        },
 
         {
         userId: 1,
-        recipeId: 3,
-        dateAdded: 12/10/2020},
+        recipeId: 3
+        },
 
         {
         userId: 2,
-        recipeId: 2,
-        dateAdded: 04/03/2015},
+        recipeId: 2
+        },
 
         {
         userId: 2,
-        recipeId: 5,
-        dateAdded: 12/10/2002},
+        recipeId: 5
+        },
 
         {
         userId: 3,
-        recipeId: 7,
-        dateAdded: 06/15/2018},
+        recipeId: 7
+        },
 
         {
         userId: 4,
-        recipeId: 9,
-        dateAdded: 07/10/2019},
+        recipeId: 9
+        },
 
         {
         userId: 4,
-        recipeId: 11,
-        dateAdded: 10/20/2020},
+        recipeId: 11
+        },
       ]);
 };
