ccfdb0051d76a314d8b7ff7c94873afa56f39529
Logan Negley <logannegley@gmail.com>
2021-04-14 13:52:59 -0500

    changed favorites select

--- Favorites/favorites-model.js
+++ Favorites/favorites-model.js
@@ -3,14 +3,14 @@ const db = require('../data/db-config');
 // Get all favorites
 function getAllFavorites(){
     return db('favoriteRecipes as fr')
-    .select('fr.favorite_id', 'fr.userId', 'fr.dateAdded', 'fr.recipeId', 'r.recipeName', 'r.image', 'r.prep_time', 'r.cook_time', 'r.isPublic')
+    .select('fr.favorite_id', 'fr.userId', 'fr.recipeId', 'r.recipeName', 'r.image', 'r.prep_time', 'r.cook_time', 'r.isPublic')
     .join('recipes as r', 'fr.recipeId', 'r.recipe_id')
 };
 
 // Get favorite by id
 function getFavoriteById(id){
     return db('favoriteRecipes as fr')
-    .select('fr.favorite_id', 'fr.userId', 'fr.dateAdded', 'fr.recipeId', 'r.recipeName', 'r.image', 'r.prep_time', 'r.cook_time', 'r.isPublic')
+    .select('fr.favorite_id', 'fr.userId', 'fr.recipeId', 'r.recipeName', 'r.image', 'r.prep_time', 'r.cook_time', 'r.isPublic')
     .join('recipes as r', 'fr.recipeId', 'r.recipe_id')
     .where('r.recipe_id', id)
 };
@@ -18,7 +18,7 @@ function getFavoriteById(id){
 // get users favorite by user Id
 function getFavoritesByUserId(id){
     return db('favoriteRecipes as fr')
-    .select('fr.favorite_id', 'fr.userId', 'fr.dateAdded', 'fr.recipeId', 'r.recipeName', 'r.image', 'r.prep_time', 'r.cook_time', 'r.isPublic')
+    .select('fr.favorite_id', 'fr.userId', 'fr.recipeId', 'r.recipeName', 'r.image', 'r.prep_time', 'r.cook_time', 'r.isPublic')
     .join('recipes as r', 'fr.recipeId', 'r.recipe_id')
     .where('fr.userId', id)
 };
