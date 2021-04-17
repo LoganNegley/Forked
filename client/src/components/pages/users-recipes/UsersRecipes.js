1bafa840f5cb26c583832a02bf32c1fde39e9d12
Logan Negley <logannegley@gmail.com>
2021-04-17 15:54:33 -0500

    added users recipe component

---
+++ client/src/components/pages/users-recipes/UsersRecipes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './users-recipes';
+
+function UsersRecipes(){
+    return(
+        <div className='users-recipe-container'>
+            <div className='triangle-box'>
+                <div id='triangle'></div>
+                <h1>P<span>ublic Recipes</span></h1>
+        </div>
+        </div>
+    )
+};
+
+export default UsersRecipes;
