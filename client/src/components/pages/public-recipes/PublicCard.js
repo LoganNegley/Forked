a6f88944dc255c8daaaf919087780774b4ce3e2b
Logan Negley <logannegley@gmail.com>
2021-04-17 15:46:29 -0500

    public recipe css done

--- client/src/components/pages/public-recipes/PublicCard.js
+++ client/src/components/pages/public-recipes/PublicCard.js
@@ -9,11 +9,15 @@ function PublicCard(props){
         <div className='public-card-wrapper'>
             {publicRecipesData.map((recipe)=>(
                 <div className='public-card' key={recipe.recipe_id}>
+                    <div className='recipe-info'>
                     {recipe.image === '' ? <img src='/images/utensils.png' alt='recipe'/> : <img src='/images/heart_icon_V3.png' alt='not available logo'/>}
-                    <h3>{recipe.recipeName}</h3>
-                    <p>Prep Time: {recipe.prep_time}</p>
-                    <p>Cook Time: {recipe.cook_time}</p>
-                    <button>View</button>
+                    <div className='recipe-details'>
+                        <h3>{recipe.recipeName}</h3>
+                        <p>Prep Time: {recipe.prep_time}</p>
+                        <p>Cook Time: {recipe.cook_time}</p>
+                    </div>
+                        <button>View</button>
+                    </div>
                 </div>
             ))}
         </div>
