bdd8d2c6830001c8541617bc2ab6de94e5b94946
Logan Negley <logannegley@gmail.com>
2021-04-20 11:59:38 -0500

    oon click handles in carousel working right

--- client/src/components/pages/user-dashboard/Carousel.js
+++ client/src/components/pages/user-dashboard/Carousel.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import './user-dashboard.css';
 
-function Carousel(){
+function Carousel({userFavorite}){
+    const [data, setData] = useState(userFavorite);
+    const [index, setIndex] = useState(0);
+    const [activeRecipe, setActiveRecipe] = useState('');
+
+
+
+    useEffect(()=>{
+        setActiveRecipe(data[index])
+        // console.log(activeRecipe)s
+    },[index])
+
+
+    const handleNext = ()=>{
+        setIndex(index + 1)
+
+    };
+
+    const handlePrev = ()=>{
+        setIndex(index - 1)
+    };
+
+
+
+
     return (
         <div className='carousel-container'>
+            <div className='carousel-wrapper'>
+                <button onClick={handlePrev}>Prev</button>
 
+                <button onClick={handleNext}>Next</button>
+            </div>
         </div>
     )
 };
