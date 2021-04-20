95b821f1915af3153bf1eaa220590de7088d2e91
Logan Negley <logannegley@gmail.com>
2021-04-20 09:44:07 -0500

    styled faves subheading

--- client/src/components/pages/user-dashboard/DashboardFaves.js
+++ client/src/components/pages/user-dashboard/DashboardFaves.js
@@ -5,12 +5,13 @@ import FavoriteCard from './FavoriteCard';
 function DashboardFaves(){
     return(
         <div className='dashboard-faves-container'>
-            <div className='circle-div'></div>
-            <h3>My Favorites</h3>
+            <div className='dashboard-section-heading'>
+                <div className='circle-div'></div>
+                <h3>My Favorites</h3>
+            </div>
             <div className='faves-wrapper'>
                 <p>Carousel Here</p>
             </div>
-
         </div>
     )
 };
