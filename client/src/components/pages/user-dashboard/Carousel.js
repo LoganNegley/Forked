8b62d336c06413d9341fc1d041eacc8a421963ec
Logan Negley <logannegley@gmail.com>
2021-04-21 16:36:00 -0500

    debugging carousel

--- client/src/components/pages/user-dashboard/Carousel.js
+++ client/src/components/pages/user-dashboard/Carousel.js
@@ -4,40 +4,54 @@ import  FavoriteCard from './FavoriteCard';
 
 function Carousel({userRecipeData}){
     const [data, setData] = useState(userRecipeData);
-    const [index, setIndex] = useState(0);
-    const [activeRecipe, setActiveRecipe] = useState('');
+    // const [sliderData, setSlider]
+    // const [index, setIndex] = useState(0);
+    // const [activeRecipe, setActiveRecipe] = useState('');
     const [offset, setOffset] = useState(0)
 
+console.log(data)
+
+const sliderData = [...data]
 
 
-    useEffect(()=>{
-        setActiveRecipe(data[index])
-    },[index])
+const infiniteSlide =() =>{
+    sliderData.push(sliderData[0])
+    sliderData.shift()
+    console.log(sliderData)
+};
 
 
-    const handleNext = ()=>{
-        setIndex(index + 1)
-        setOffset(-Math.abs(index*(100/data.length)))
-    };
 
-    console.log(offset)
 
-    const handlePrev = ()=>{
-        setIndex(index - 1)
-        setOffset(index*(100/data.length))
-    };
+    // useEffect(()=>{
+    //     setActiveRecipe(data[index])
+    // },[index])
+
+    // setInterval(()=>{
+    //     setOffset(offset - 330)
+    //     infiniteSlide()
+    // },3000)
+
+
+    // const handleNext = ()=>{
+    //     setIndex(index + 1)
+    //     setOffset(-Math.abs(index*(100/data.length)))
+    // };
+
+
+    // const handlePrev = ()=>{
+    //     setIndex(index - 1)
+    //     setOffset(index*(100/data.length))
+    // };
 
     return (
         <div className='carousel-container'>
-            <div className='carousel-btns'>
-                <button onClick={handlePrev}>Prev</button>
-                <button onClick={handleNext}>Next</button>
-            </div>
             <div className='card-slider'>
-                <div className='card-slider-wrapper' style={{'transform':`translateX(${offset}%)`}}>
-                    <FavoriteCard data={data} index={index}/>
+                <div className='card-slider-wrapper' style={{'transform':`translateX(${offset}px)`, 'transition': '2s'}}>
+                    <FavoriteCard data={sliderData}/>
                 </div>
             </div>
+            <button onClick={infiniteSlide}></button>
         </div>
     )
 };
--- client/src/components/pages/user-dashboard/FavoriteCard.js
+++ client/src/components/pages/user-dashboard/FavoriteCard.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-function FavoriteCard({index,data}){
+function FavoriteCard({data}){
 
     return (
         <>
       {      data.map(item =>(
 
-        <div key={item.favorite_id} id={`card-${index}`} className='favorite-card'>
+        <div key={item.favorite_id} className='favorite-card'>
         <div className='favorite-card-info'>
           {item.image !== '' ? <img src='/images/utensils.png' alt='recipe holder'/> : <img src={item.image} alt='recipe'/> }
-            
                 <p>{item.recipeName}</p>
                 <p>Prep Time: {item.prep_time}</p>
                 <p>Cook Time: {item.cook_time}</p>
