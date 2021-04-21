dce911e8d206b0d62c397172d2d9566b92d2d732
Logan Negley <logannegley@gmail.com>
2021-04-21 09:54:51 -0500

    styles and most of the functionality working with carousel

--- client/src/components/pages/user-dashboard/Carousel.js
+++ client/src/components/pages/user-dashboard/Carousel.js
@@ -6,6 +6,7 @@ function Carousel({userRecipeData}){
     const [data, setData] = useState(userRecipeData);
     const [index, setIndex] = useState(0);
     const [activeRecipe, setActiveRecipe] = useState('');
+    const [offset, setOffset] = useState(0)
 
 
 
@@ -16,15 +17,15 @@ function Carousel({userRecipeData}){
 
     const handleNext = ()=>{
         setIndex(index + 1)
-
+        setOffset(-Math.abs(index*(100/data.length)))
     };
+
+    console.log(offset)
 
     const handlePrev = ()=>{
         setIndex(index - 1)
+        setOffset(index*(100/data.length))
     };
-
-
-
 
     return (
         <div className='carousel-container'>
@@ -33,7 +34,7 @@ function Carousel({userRecipeData}){
                 <button onClick={handleNext}>Next</button>
             </div>
             <div className='card-slider'>
-                <div className='card-slider-wrapper'>
+                <div className='card-slider-wrapper' style={{'transform':`translateX(${offset}%)`}}>
                     <FavoriteCard data={data} index={index}/>
                 </div>
             </div>
--- client/src/components/pages/user-dashboard/FavoriteCard.js
+++ client/src/components/pages/user-dashboard/FavoriteCard.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
 function FavoriteCard({index,data}){
-    // const {cook_time, favorite_id, image, prep_time, recipeId, recipeName} = activeRecipe;
 
     return (
         <>
       {      data.map(item =>(
+
         <div key={item.favorite_id} id={`card-${index}`} className='favorite-card'>
+        <div className='favorite-card-info'>
           {item.image !== '' ? <img src='/images/utensils.png' alt='recipe holder'/> : <img src={item.image} alt='recipe'/> }
-            <div className='favorite-card-info'>
+            
                 <p>{item.recipeName}</p>
                 <p>Prep Time: {item.prep_time}</p>
                 <p>Cook Time: {item.cook_time}</p>
             </div>
         </div>
-
       ))
       }
       </>
