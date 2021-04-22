4490679fb126be92c6b8e944698ec4c275e8e4a0
Logan Negley <logannegley@gmail.com>
2021-04-22 14:20:56 -0500

    restructure components to be dynamic

--- client/src/components/pages/user-dashboard/DashboardPublic.js
+++ client/src/components/pages/user-dashboard/DashboardPublic.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Carousel from './Carousel';
+import axios from 'axios';
 
 function DashboardPublic(){
+    const [publicRecipe, setPublicRecipes] = useState('');
+
+    useEffect(() =>{
+        axios.get('http://localhost:5000/public/recipes/')
+        .then(res =>{
+            console.log(res.data)
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    },[]);
+
     return(
         <div className='dashboard-section-container'>
             <div className='dashboard-section-heading'>
                 <div className='circle-div'></div>
-                <h3>Inspiration</h3>
+                <h3>Get Inspired</h3>
             </div>
             <div className='public-section-wrapper'>
-                <div className='image-card'>
-                    
-                </div>
+                {/* <Carousel/> */}
             </div>
         </div>
     )
