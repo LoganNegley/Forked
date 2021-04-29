00d251433a257728eca7d5d55d625172c97eacf5
Logan Negley <logannegley@gmail.com>
2021-04-29 17:13:55 -0500

    image uploader started

---
+++ client/src/components/pages/add-recipe/ImageUploader.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+function ImageUploader(){
+    return(
+        <div className='dropzone'>
+
+        </div>
+    )
+};
+
+export default ImageUploader;
