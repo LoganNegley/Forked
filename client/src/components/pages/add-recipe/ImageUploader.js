aa638fe9f3f14be984ecfe3f114c6d720d22ab6e
Logan Negley <logannegley@gmail.com>
2021-04-30 09:54:15 -0500

    working on image uploader for adding recipe

--- client/src/components/pages/add-recipe/ImageUploader.js
+++ client/src/components/pages/add-recipe/ImageUploader.js
@@ -1,10 +1,22 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import Dropzone from 'react-dropzone';
 
 function ImageUploader(){
+
+    const handleDrop =(files) =>{
+        const image = files[0]
+
+        const url =
+    }
+
+
+
     return(
         <div className='dropzone'>
+            <Dropzone
 
+            />
         </div>
     )
 };
