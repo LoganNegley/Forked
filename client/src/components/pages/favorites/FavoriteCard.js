b9f96321ed6934b618246d4e29c68df599a1fb25
Logan Negley <logannegley@gmail.com>
2021-05-05 13:54:07 -0500

    favorite recipe cards made for users

--- client/src/components/pages/favorites/FavoriteCard.js
+++ client/src/components/pages/favorites/FavoriteCard.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import './FavoriteCard.css';
 
-function FavoriteCard(){
+
+function FavoriteCard(props){
+    const recipe = props.item;
+
     return(
         <div className='favorite-card-container'>
-            
+            <div id='fav-col-1'>
+                {recipe.image === null ? <img src='/images/utensils.png' alt='not available' /> :
+                <img src={recipe.image} alt='recipe'/>}
+            </div>
+            <div id='fav-col-2'>
+                <h3>{recipe.recipeName}</h3>
+                <p>Prep Time: {recipe.prep_time}</p>
+                <p>Cook Time: {recipe.cook_time}</p>
+            </div>
+            <div id='fav-col-3'>
+                <button>View</button>
+                <button>Remove</button>
+            </div>
         </div>
     )
 };
--- client/src/components/pages/favorites/UserFavorite.js
+++ client/src/components/pages/favorites/UserFavorite.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './user-favorites.css';
+import axios from 'axios';
+import {Link, useParams} from 'react-router-dom';
+import FavoriteCard from './FavoriteCard';
 
 function UserFavorite(){
+    const {userId} = useParams();
+    const [recipes, setRecipes] = useState('')
+
+
+    useEffect(() =>{
+        axios.get(`http://localhost:5000/favorites/user/${userId}`)
+        .then(res =>{
+            setRecipes(res.data)
+        })
+        .catch(error =>{
+            console.log(error)
+        })
+    },[])
+
+    console.log(recipes)
+
+    if(!recipes){
+        return(
+            <div>You don't have any favorite recipes saved YET!!!</div>
+        )
+    }
+
+
     return (
         <div className='favorite-container'>
             <div className='triangle-box'>
                 <div className='heading-triangle'></div>
                 <h1>P<span>ublic Recipes</span></h1>
             </div>
-
+            {recipes.map((item, index) =>(
+                <FavoriteCard key={index} item={item}/>
+            ))}
+            
         </div>
     )
 };
