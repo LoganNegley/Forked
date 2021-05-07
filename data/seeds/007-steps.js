a0b9d2ef70f8f1082b6f0bcd6da0db950795f912
Logan Negley <logannegley@gmail.com>
2021-05-07 16:00:32 -0500

    changed seeding

--- data/seeds/007-steps.js
+++ data/seeds/007-steps.js
@@ -1,108 +1,55 @@
 
 exports.seed = function(knex, Promise) {
       return knex('steps').insert([
-        {instruction: 'ground meat in skillet',
+        {instruction: 'Add the beef to a large skillet over medium-high heat. Break the meat apart with a wooden spoon. Add the chili powder, cumin, salt, oregano, garlic powder, and pepper to the meat. Stir well. Cook until the meat is cooked through, about 6-8 minutes, stirring occasionally.',
         step_number: 1,
         recipeId: 1},
 
-        {instruction: 'add taco seansoning ',
+        {instruction: 'Reduce the heat to medium. Add the tomato sauce and water. Stir to combine. Cook, stirring occasionally, for 7-8 minutes, until some of the liquid evaporates but the meat mixture is still a little saucy. Remove from the heat.',
         step_number: 2,
         recipeId: 1},
 
-        {instruction: 'add cake mix in a bowl',
+        {instruction: 'Warm the taco shells according to their package directions.',
+        step_number: 3,
+        recipeId: 1},
+
+        {instruction: 'Fill the taco shells with 2 heaping tablespoons of taco meat. Top with desired taco toppings: shredded cheese, shredded lettuce, chopped tomatoes, diced red onion, taco sauce, sour cream, guacamole, etc',
+        step_number: 4,
+        recipeId: 1},
+
+
+        {instruction: 'Heat oven to 375°F. In small bowl, mix flour, baking soda and salt; set aside.',
         step_number: 1,
         recipeId: 2},
 
-        {instruction: 'add egg to bowl and mix',
+        {instruction: 'In large bowl, beat softened butter and sugars with electric mixer on medium speed, or mix with spoon about 1 minute or until fluffy, scraping side of bowl occasionally.',
         step_number: 2,
         recipeId: 2},
 
-        {instruction: 'season chicken breast',
+        {instruction: 'Beat in egg and vanilla until smooth. Stir in flour mixture just until blended (dough will be stiff). Stir in chocolate chips and nuts',
+        step_number: 3,
+        recipeId: 2},
+
+        {instruction: 'Onto ungreased cookie sheets, drop dough by rounded tablespoonfuls 2 inches apart',
+        step_number: 4,
+        recipeId: 2},
+        
+        {instruction: 'Bake 8 to 10 minutes or until light brown (centers will be soft). Cool 2 minutes; remove from cookie sheet to cooling rack. Cool completely, about 30 minutes. Store covered in airtight container.',
+        step_number: 5,
+        recipeId: 2},
+
+
+
+        {instruction: 'In a blender, combine the strawberries, milk and ice cream.',
         step_number: 1,
         recipeId: 3},
 
-        {instruction: 'place on pan and place in oven at 425 deg',
+        {instruction: 'Pureé until thick and creamy',
         step_number: 2,
         recipeId: 3},
 
-        {instruction: 'season fish to taste',
-        step_number: 1,
-        recipeId: 4},
-
-        {instruction: 'heat skillet to temp',
-        step_number: 2,
-        recipeId: 4},
-
-        {instruction: 'place fish on skillet 5 min each side',
+        {instruction: 'garnish with whipped cream. Serve.',
         step_number: 3,
-        recipeId: 4},
-
-        {instruction: 'place on plate and server with lemon',
-        step_number: 4,
-        recipeId: 4},
-
-        {instruction: 'cut cabbage in pieces',
-        step_number: 1,
-        recipeId: 5},
-
-        {instruction: 'pour cerial into medium bowl',
-        step_number: 1,
-        recipeId: 6},
-
-        {instruction: 'add milk on top of cerial and eat',
-        step_number: 2,
-        recipeId: 6},
-
-        {instruction: 'mix cheees and frozen potatoes together',
-        step_number: 1,
-        recipeId: 7},
-        
-        {instruction: 'add to pan',
-        step_number: 2,
-        recipeId: 7},
-        
-        {instruction: 'bake in oven for 45 mins',
-        step_number: 3,
-        recipeId: 7},
-
-        {instruction: 'place hotdogs into boiling water in pot',
-        step_number: 1,
-        recipeId: 8},
-
-        {instruction: 'ground vegi meat in skillet',
-        step_number: 1,
-        recipeId: 9},
-
-        {instruction: 'add seasoning',
-        step_number: 2,
-        recipeId: 9},
-
-        {instruction: 'cut potatoes into quarters',
-        step_number: 1,
-        recipeId: 10},
-        
-        {instruction: 'gorund meat in skillet',
-        step_number: 2,
-        recipeId: 10},
-
-        {instruction: 'place noodles in bowl add water',
-        step_number: 1,
-        recipeId: 11},
-
-        {instruction: 'heat up meat in skillet',
-        step_number: 1,
-        recipeId: 12},
-
-        {instruction: 'place onion and green pepper on skillet',
-        step_number: 2,
-        recipeId: 12},
-
-        {instruction: 'place on bunn',
-        step_number: 3,
-        recipeId: 12},
-
-        {instruction: 'add cheese to the top',
-        step_number: 4,
-        recipeId: 12},
+        recipeId: 3},
       ]);
 };
--- data/seeds/008-publicRecipes.js
+++ data/seeds/008-publicRecipes.js
@@ -5,35 +5,10 @@ exports.seed = function(knex, Promise) {
         userId: 1,
         recipeId: 1
         },
-
         {
         userId: 1,
-        recipeId: 3
-        },
-
-        {
-        userId: 2,
         recipeId: 2
         },
 
-        {
-        userId: 2,
-        recipeId: 5
-        },
-
-        {
-        userId: 3,
-        recipeId: 7
-        },
-
-        {
-        userId: 4,
-        recipeId: 9
-        },
-
-        {
-        userId: 4,
-        recipeId: 11
-        },
       ]);
 };
