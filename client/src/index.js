6d83a3f8e008b09a819a24c60ce9c5ece776e88d
Logan Negley <logannegley@gmail.com>
2021-05-08 00:32:03 -0500

    debug

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <HashRouter basename='/index.html'>
+  <BrowserRouter basename='/index.html'>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
